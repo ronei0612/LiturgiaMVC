f66231098db09e48b044e353f263ae1a8434c7f9
Ronei Costa Soares <ronei.costa@aplicativo.net>
2023-07-01 16:53:14 -0300

    javascript

--- LiturgiaMVC/wwwroot/js/site.js
+++ LiturgiaMVC/wwwroot/js/site.js
@@ -45,3 +45,23 @@ function gerarLink() {
 
 	location.replace(base + link);
 }
+
+window.onload = function () {
+};
+
+/* Remover Comercial */
+$(document).ready(function () {
+	$("div[style='opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;']").remove();
+	$("div[style='margin: 0px; padding: 0px; left: 0px; width: 100%; height: 65px; right: 0px; bottom: 0px; display: block; position: fixed; z-index: 2147483647; opacity: 0.9; background-color: rgb(32, 32, 32);']").remove();
+	$("div[onmouseover='S_ssac();']").remove();
+	$("center").remove();
+
+	// Adapta ao celular
+	if ($(window).width() <= 699) {
+		$('img')
+			.css('max-width', '100%')
+			.css('min-width', '100%')
+			.css('object-fit', 'fill')
+			.css('height', 'auto');
+	}
+});
