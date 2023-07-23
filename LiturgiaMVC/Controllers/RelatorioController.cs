9e9ed76e608b810d552ab8ec73c8341b9b56cb3f
Ronei Costa Soares <ronei0612@gmail.com>
2023-07-23 15:39:46 -0300

    Relatório

---
+++ LiturgiaMVC/Controllers/RelatorioController.cs
@@ -0,0 +1,20 @@
+﻿using LiturgiaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiturgiaMVC.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        public IActionResult Index()
+        {
+            var lines = System.IO.File.ReadAllLines(Variaveis.arquivoIPs);            
+
+            var linksModel = new LinksModel
+            {
+                RelatorioLines = lines
+            };
+
+            return View(linksModel);
+        }
+    }
+}
--- LiturgiaMVC/Models/LinksModel.cs
+++ LiturgiaMVC/Models/LinksModel.cs
@@ -3,7 +3,9 @@
     public class LinksModel
     {
         public Dictionary<string, string>? LinksDict { get; set; }
-        public string[]? Youtube { get; set; }		
+        public string[]? Youtube { get; set; }
 		public Dictionary<string, string>? YoutubeImageLinks { get; set; }
-	}
+
+        public string[]? RelatorioLines { get; set; }
+    }
 }
