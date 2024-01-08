dafe9ad136f91963b5b41b9ac5c88daf09ddae97
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-01-08 09:27:51 -0300

    removido função que eu estava testando

--- LiturgiaMVC/Controllers/OrgaoController.cs
+++ LiturgiaMVC/Controllers/OrgaoController.cs
@@ -42,37 +42,6 @@ namespace LiturgiaMVC.Controllers
             return View(linksModel);
         }
 
-        private IEnumerable<string> ExtractText(CObject cObject)
-        {
-            var textList = new List<string>();
-            if (cObject is COperator)
-            {
-                var cOperator = cObject as COperator;
-                if (cOperator.OpCode.Name == OpCodeName.Tj.ToString() ||
-                    cOperator.OpCode.Name == OpCodeName.TJ.ToString())
-                {
-                    foreach (var cOperand in cOperator.Operands)
-                    {
-                        textList.AddRange(ExtractText(cOperand));
-                    }
-                }
-            }
-            else if (cObject is CSequence)
-            {
-                var cSequence = cObject as CSequence;
-                foreach (var element in cSequence)
-                {
-                    textList.AddRange(ExtractText(element));
-                }
-            }
-            else if (cObject is CString)
-            {
-                var cString = cObject as CString;
-                textList.Add(cString.Value);
-            }
-            return textList;
-        }
-
         public IActionResult Gravacao()
         {
             return View();
