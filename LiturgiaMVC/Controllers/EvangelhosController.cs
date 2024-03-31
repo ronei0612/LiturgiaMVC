a7be7c9e2d8da66651a95be3b61803ae3c0b1f37
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-03-31 19:10:20 -0300

    Evangelhos cartas e apócrifos

---
+++ LiturgiaMVC/Controllers/EvangelhosController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiturgiaMVC.Controllers
+{
+    public class EvangelhosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cartas()
+        {
+            return View();
+        }
+
+        public IActionResult Evangelhos()
+        {
+            return View();
+        }
+
+        public IActionResult Apocrifos()
+        {
+            return View();
+        }
+
+        public IActionResult Mateus()
+        {
+            return View();
+        }
+
+        public IActionResult Marcos()
+        {
+            return View();
+        }
+
+        public IActionResult Lucas()
+        {
+            return View();
+        }
+
+        public IActionResult Joao()
+        {
+            return View();
+        }
+    }
+}
